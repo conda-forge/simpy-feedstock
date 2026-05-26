0c6e3251da33cb472d38cd2a112f98ada2973191
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-05-25 22:10:09 -0500

    fix test

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,23 +1,31 @@
 import platform
-import subprocess
+from subprocess import call
 import sys
 
-PYTEST_ARGS = ["-vv"] + dict(
-    # measure coverage
-    Linux=[
-        "--cov",
-        "simpy",
-        "--cov-report",
-        "term-missing:skip-covered",
-        "--cov-fail-under",
-        "92",
-    ],
+COV_FAIL_UNDER = 95
+COV_ARGS = ["coverage", "run", "--source=simpy", "--branch"]
+OS = platform.system()
+PYTEST_ARGS = [
+    "-vv",
+    "--tb=long",
+    "--color=yes",
+]
+OS_ARGS = {
     # too slow for realtime tests
-    Darwin=["-k", "not(test_rt)"],
+    "Darwin": ["-k", "not(test_rt)"],
     # test assumes POSIX paths... and is too slow
-    Windows=["-k", "not(exception_chaining or test_rt)"],
-)[platform.system()]
+    "Windows": ["-k", "not(exception_chaining or test_rt)"],
+}
+REPORT_ARGS = [
+    "coverage",
+    "report",
+    "--show-missing",
+    "--skip-covered",
+    f"--fail-under={COV_FAIL_UNDER}",
+]
 
 if __name__ == "__main__":
-    print("Running pytest with", " ".join(PYTEST_ARGS), flush=True)
-    sys.exit(subprocess.call(["pytest", *PYTEST_ARGS], cwd="tests"))
+    sys.exit(
+        call([*COV_ARGS, "-m", "pytest", *PYTEST_ARGS, *OS_ARGS.get(OS, [])])
+        or (call(REPORT_ARGS) if OS == "Linux" else 0)
+    )
